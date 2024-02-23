61cc18fedbd8b188d3c138810cd45aa41c04ab54
Dmitry <mitya.shabat@gmail.com>
2024-02-23 14:07:07 +0400

    Update utils

--- othermath.py
+++ othermath.py
@@ -1,11 +1,16 @@
 import numpy as np
 import math
+import torch
+
+
 def angle_between(vector_a, vector_b):
-    costheta = vector_a.dot(vector_b) / (np.linalg.norm(vector_a)*np.linalg.norm(vector_b))
+    costheta = vector_a.dot(vector_b) / (np.linalg.norm(vector_a) * np.linalg.norm(vector_b))
     return math.acos(costheta)
+
 
 def cot(theta):
     return math.cos(theta) / math.sin(theta)
+
 
 def string_is_int(string):
     try:
@@ -14,16 +19,17 @@ def string_is_int(string):
     except ValueError:
         return False
 
+
+def to_homogeneous(v):
+    if type(v) is np.ndarray:
+        v = np.concatenate((v, np.ones((*v.shape[:-1], 1))), axis=-1)
+    elif type(v) is torch.Tensor:
+        v = torch.cat((v, torch.ones((*v.shape[:-1], 1)).to(v.device)), dim=-1)
+    return v
+
+
 def inf_norm(matrix):
     return np.amax(np.abs(matrix))
+
+
 # Apply the 4x4 matrix to the 1x3 vector
-def apply_rotation(rotation_matrix, vector):
-    is_4_by_4 = rotation_matrix.size == 16
-    if(is_4_by_4):
-        vector = np.append(vector, 1)
-    vector = rotation_matrix.dot(np.matrix(vector).transpose())
-    vector = vector.transpose()
-    if(is_4_by_4):
-        # then remove last element
-        vector = np.delete(vector, 3)
-    return np.squeeze(np.asarray(vector))
